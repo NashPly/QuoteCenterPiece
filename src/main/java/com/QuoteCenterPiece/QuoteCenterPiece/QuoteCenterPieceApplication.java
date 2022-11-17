9e69f68908fe29b41f4aa2568be8749e4bffe87b
NashPly <tbeals@nashvilleplywood.com>
2022-11-17 11:34:00 -0600

    Imported Project

---
+++ src/main/java/com/QuoteCenterPiece/QuoteCenterPiece/QuoteCenterPieceApplication.java
@@ -0,0 +1,13 @@
+package com.QuoteCenterPiece.QuoteCenterPiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteCenterPieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteCenterPieceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QuoteCenterPiece/QuoteCenterPiece/QuoteCenterPieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuoteCenterPiece.QuoteCenterPiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteCenterPieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
